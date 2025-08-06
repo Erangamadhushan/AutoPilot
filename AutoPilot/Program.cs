8a70f0952560f17aa9efca634eddee7523986863
Erangamadhushan <erangamadhushann956@gmail.com>
2025-08-06 23:14:16 +0530

    Initial set up created

---
+++ AutoPilot/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoPilot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to AutoPilot!");
+            Console.WriteLine("This is a simple console application to demonstrate basic functionality.");
+            Console.WriteLine("Press any key to exit...");
+            // Wait for user input before closing
+            Console.ReadKey();
+        }
+    }
+}
